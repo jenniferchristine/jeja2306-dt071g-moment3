d4136891d6e7c198aacef481f20d6095b7d67b52
your.user.name <jeja2306@student.miun.se>
2025-09-26 10:57:08 +0200

    update

--- obj/Debug/net8.0/jeja2306-dt071g-moment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/jeja2306-dt071g-moment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jeja2306-dt071g-moment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1a7fda535cb4ebdb5d5a6909b8ffc9c8645a0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7d0eaf2a434add3c840776eacea041942ed322")]
 [assembly: System.Reflection.AssemblyProductAttribute("jeja2306-dt071g-moment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jeja2306-dt071g-moment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
